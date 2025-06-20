aeb2d84ba9fb8bfee77e8076cba8590f72404554
theEvilReaper <steffenwx@gmail.com>
2025-06-20 10:27:55 +0200

    More listener rework

--- src/main/java/net/theevilreaper/bounce/listener/PlayerQuitListener.java
+++ src/main/java/net/theevilreaper/bounce/listener/PlayerQuitListener.java
@@ -1,20 +1,53 @@
 package net.theevilreaper.bounce.listener;
 
+import net.kyori.adventure.audience.Audience;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
 import net.theevilreaper.bounce.Bounce;
 import net.theevilreaper.bounce.profile.BounceProfile;
+import net.theevilreaper.bounce.timer.LobbyPhase;
+import net.theevilreaper.bounce.util.GameMessages;
+import net.theevilreaper.xerus.api.phase.LinearPhaseSeries;
+import net.theevilreaper.xerus.api.phase.Phase;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
-public class PlayerQuitListener implements Listener {
+import static net.minestom.server.MinecraftServer.getConnectionManager;
 
-    private final Bounce game;
+public class PlayerQuitListener implements Consumer<PlayerDisconnectEvent> {
 
-    public PlayerQuitListener(Bounce game) {
-        this.game = game;
+    private final Supplier<Phase> phaseSupplier;
+
+    public PlayerQuitListener(@NotNull Supplier<Phase> phaseSupplier) {
+        this.phaseSupplier = phaseSupplier;
+    }
+
+    @Override
+    public void accept(@NotNull PlayerDisconnectEvent event) {
+        Phase phase = phaseSupplier.get();
+        Player player = event.getPlayer();
+
+        if (phase instanceof LobbyPhase lobbyPhase) {
+            this.handleLobbyQuit(lobbyPhase,player);
+        }
+    }
+
+    /**
+     * Handles the quit logic for the {@link LobbyPhase}.
+     *
+     * @param lobbyPhase the reference from the phase
+     * @param player     the player which is involved
+     */
+    private void handleLobbyQuit(@NotNull LobbyPhase lobbyPhase, @NotNull Player player) {
+        lobbyPhase.checkStopCondition();
+        Audience.audience(getConnectionManager().getOnlinePlayers()).sendMessage(GameMessages.getLeaveMessage(player));
     }
 
     @EventHandler
