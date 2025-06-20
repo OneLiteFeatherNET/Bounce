de3cae157d7f688e09050dd76082d2c7fdd254fe
theEvilReaper <steffenwx@gmail.com>
2025-06-20 10:28:24 +0200

    Update the StartCommand implementation

--- src/main/java/net/theevilreaper/bounce/commands/StartCommand.java
+++ src/main/java/net/theevilreaper/bounce/commands/StartCommand.java
@@ -1,35 +1,61 @@
 package net.theevilreaper.bounce.commands;
 
-import net.theevilreaper.bounce.Bounce;
-import net.titan.spigot.Cloud;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import net.theevilreaper.bounce.common.config.GameConfig;
+import net.theevilreaper.bounce.common.util.Messages;
+import net.theevilreaper.bounce.timer.LobbyPhase;
+import net.theevilreaper.bounce.util.GameMessages;
+import net.theevilreaper.xerus.api.phase.Phase;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.command.builder.condition.Conditions;
+import org.jetbrains.annotations.NotNull;
 
-public class StartCommand implements CommandExecutor {
+import java.util.function.Supplier;
 
-    private final Bounce game;
+/**
+ * The command allows to force start the game.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+public class StartCommand extends net.minestom.server.command.builder.Command {
 
-    public StartCommand(Bounce game) {
-        this.game = game;
+    private final Supplier<Phase> phaseSupplier;
+    private final Component unableToStart = Messages.withPrefix(
+            Component.text("Unable to start the game because the timer is to low!", NamedTextColor.RED)
+    );
+
+    public StartCommand(@NotNull Supplier<Phase> phaseSupplier) {
+        super("start");
+        this.phaseSupplier = phaseSupplier;
+        this.setCondition(Conditions::playerOnly);
+        addSyntax(this::forceStart);
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            return false;
-        } else {
-            Player player = (Player)sender;
+    private void forceStart(@NotNull net.minestom.server.command.CommandSender sender, @NotNull CommandContext commandContext) {
+        Phase phase = phaseSupplier.get();
 
-            if (!Cloud.getInstance().getPlayer(player).hasPermission("game.bounce.start")) return false;
+        if (!(phase instanceof LobbyPhase lobbyPhase)) return;
 
-            if (game.getLobbyTimer().isRunning()) {
-                game.getLobbyTimer().setTime(10);
-                player.sendMessage(game.getPrefix() + "§cDas Spiel startet sofort");
-                return true;
-            }
+        if (lobbyPhase.isPaused()) {
+            sender.sendMessage(GameMessages.PHASE_NOT_RUNNING);
+            return;
         }
-        return false;
+
+        if (lobbyPhase.getCurrentTicks() < GameConfig.FORCE_START_TIME + 1) {
+            sender.sendMessage(unableToStart);
+            return;
+        }
+
+        if (lobbyPhase.isForceStarted()) {
+            sender.sendMessage(GameMessages.ALREADY_FORCE_STARTED);
+            return;
+        }
+
+        lobbyPhase.setForceStarted(true);
+        sender.sendMessage(GameMessages.PHASE_FORCE_STARTED);
     }
 }
+
