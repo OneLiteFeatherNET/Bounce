6c56a69f08c836641e2d2955ed1d98ab3f2f071c
theEvilReaper <steffenwx@gmail.com>
2025-06-20 13:17:09 +0200

    Add test dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -13,5 +13,13 @@ dependencies {
     compileOnly(libs.minestom)
     compileOnly(libs.aves)
     compileOnly(libs.xerus)
+
+    testImplementation(libs.minestom)
+    testImplementation(libs.aves)
+    testImplementation(libs.cyano)
+    testImplementation(libs.junit.api)
+    testImplementation(libs.junit.params)
+    testImplementation(libs.junit.platform.launcher)
+    testRuntimeOnly(libs.junit.engine)
 }
 
