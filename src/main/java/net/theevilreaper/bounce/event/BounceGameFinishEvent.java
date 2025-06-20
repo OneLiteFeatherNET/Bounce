42f5d0e313bf5e7b4765fda2402fea1d2bdaa77d
theEvilReaper <steffenwx@gmail.com>
2025-06-20 13:57:13 +0200

    Add documentation

--- src/main/java/net/theevilreaper/bounce/event/BounceGameFinishEvent.java
+++ src/main/java/net/theevilreaper/bounce/event/BounceGameFinishEvent.java
@@ -3,12 +3,28 @@ package net.theevilreaper.bounce.event;
 import net.theevilreaper.xerus.api.event.GameFinishEvent;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The {@link BounceGameFinishEvent} extends the {@link GameFinishEvent} to provide specific reasons for finishing a Bounce game.
+ * It includes reasons such as time over, one player left, or a player left the game.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public class BounceGameFinishEvent extends GameFinishEvent<BounceGameFinishEvent.Reason> {
 
+    /**
+     * Constructs a new BounceGameFinishEvent with the specified reason.
+     *
+     * @param reason the reason for finishing the game
+     */
     public BounceGameFinishEvent(@NotNull Reason reason) {
         super(reason);
     }
 
+    /**
+     * Enum representing the reasons for finishing a Bounce game.
+     */
     public enum Reason {
 
         TIME_OVER,
