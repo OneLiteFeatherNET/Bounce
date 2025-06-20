55acc5e0edb730df60bc1742332ef4a9f4c96f7f
theEvilReaper <steffenwx@gmail.com>
2025-06-20 10:28:37 +0200

    Various changes

--- src/main/java/net/theevilreaper/bounce/Bounce.java
+++ src/main/java/net/theevilreaper/bounce/Bounce.java
@@ -1,53 +1,66 @@
 package net.theevilreaper.bounce;
 
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.event.Event;
+import net.minestom.server.event.EventNode;
+import net.minestom.server.event.GlobalEventHandler;
 import net.theevilreaper.bounce.commands.StartCommand;
-import com.theEvilReaper.bounce.listener.*;
-import net.theevilreaper.bounce.listener.*;
+import net.theevilreaper.bounce.common.ListenerHandling;
+import net.theevilreaper.bounce.common.config.GameConfig;
+import net.theevilreaper.bounce.common.config.GameConfigReader;
 import net.theevilreaper.bounce.map.MapManager;
 import net.theevilreaper.bounce.profile.BounceProfile;
-import net.theevilreaper.bounce.timer.InGameTimer;
-import net.theevilreaper.bounce.timer.LobbyTimer;
-import net.theevilreaper.bounce.timer.RestartTimer;
+import net.theevilreaper.bounce.timer.PlayingPhase;
+import net.theevilreaper.bounce.timer.LobbyPhase;
+import net.theevilreaper.bounce.timer.RestartPhase;
+import net.theevilreaper.bounce.timer.TeleportPhase;
 import net.theevilreaper.bounce.util.GameUtil;
 import net.theevilreaper.bounce.util.ItemUtil;
 import net.theevilreaper.bounce.util.ScoreboardUtil;
-import de.icevizion.api.profile.ProfileService;
-import de.icevizion.api.world.WorldHelper;
-import de.icevizion.game.Game;
-import org.bukkit.Bukkit;
-import org.bukkit.World;
+import net.theevilreaper.xerus.api.phase.LinearPhaseSeries;
+import net.theevilreaper.xerus.api.phase.Phase;
+import org.jetbrains.annotations.NotNull;
 
-public class Bounce extends Game {
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Bounce implements ListenerHandling {
 
     private MapManager mapManager;
     private ProfileService<BounceProfile> profileService;
-
+    private GameConfig gameConfig;
     private ScoreboardUtil scoreboardUtil;
     private GameUtil gameUtil;
     private ItemUtil itemUtil;
+    private final LinearPhaseSeries<Phase> phaseSeries;
 
-    private LobbyTimer lobbyTimer;
-    private InGameTimer gameTimer;
-    private RestartTimer restartTimer;
+    public Bounce() {
+        Path path = Paths.get("");
+        this.gameConfig = new GameConfigReader(path.resolve("config")).getConfig();
 
-    @Override
+        this.phaseSeries = new LinearPhaseSeries<>();
+
+
+        this.registerCommands();
+        this.registerPhases();
+    }
+
     public void load() {
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        registerCancelListener(globalEventHandler);
         setPrefix("§7[§3Bounce§7] §r");
         WorldHelper.disablePlayerFileSave();
-        updateGameRules();
         scoreboardUtil = new ScoreboardUtil();
         mapManager = new MapManager(this);
         itemUtil = new ItemUtil();
         gameUtil = new GameUtil(this);
         profileService = new ProfileService<>();
-        registerTimer();
         registerCommands();
         registerListener();
-        lobbyTimer.start();
-       // Cloud.getInstance().setSpigotState(SpigotState.AVAILABLE);
+        lobbyPhase.start();
+        // Cloud.getInstance().setSpigotState(SpigotState.AVAILABLE);
     }
 
-    @Override
     public void unload() {
         //Cloud.getInstance().setSpigotState(SpigotState.END);
         scoreboardUtil = null;
@@ -58,32 +71,18 @@ public class Bounce extends Game {
         profileService = null;
     }
 
-    private void updateGameRules() {
-        for (World world : Bukkit.getWorlds()) {
-            world.setGameRuleValue("doDaylightCycle", "false");
-            world.setGameRuleValue("doFireTick", "false");
-        }
+    private void registerPhases() {
+        this.phaseSeries.add(new LobbyPhase(this.gameConfig.minPlayers(), this.gameConfig.maxPlayers(), this.gameConfig.lobbyTime()));
+        this.phaseSeries.add(new TeleportPhase(this.itemUtil, player -> { }));
+        this.phaseSeries.add(new PlayingPhase(value -> {}));
+        this.phaseSeries.add(new RestartPhase());
     }
 
-    private void registerTimer() {
-        lobbyTimer = new LobbyTimer(this);
-        gameTimer = new InGameTimer(this);
-        restartTimer = new RestartTimer(this);
-    }
-
-    private void registerListener() {
-        getServer().getPluginManager().registerEvents(new PlayerBlockListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerDamageListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerDropListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerFoodListener(), this);
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), this);
-        getServer().getPluginManager().registerEvents(new PlayerSpawnListener(mapManager), this);
-        getServer().getPluginManager().registerEvents(new WorldListener(), this);
+    private void registerListener(@NotNull EventNode<Event> node) {
     }
 
     private void registerCommands() {
-        getCommand("start").setExecutor(new StartCommand(this));
+        MinecraftServer.getCommandManager().register(new StartCommand(this.phaseSeries::getCurrentPhase));
     }
 
     public MapManager getMapManager() {
@@ -106,15 +105,15 @@ public class Bounce extends Game {
         return profileService;
     }
 
-    public LobbyTimer getLobbyTimer() {
-        return lobbyTimer;
+    public LobbyPhase getLobbyTimer() {
+        return lobbyPhase;
     }
 
-    public InGameTimer getGameTimer() {
+    public PlayingPhase getGameTimer() {
         return gameTimer;
     }
 
-    public RestartTimer getRestartTimer() {
+    public RestartPhase getRestartTimer() {
         return restartTimer;
     }
 }
