428d8ee8c5e1bbd7a27e23cb273ed50545e3cdad
theEvilReaper <steffenwx@gmail.com>
2025-06-20 12:47:34 +0200

    Update message part

--- src/main/java/net/theevilreaper/bounce/util/GameMessages.java
+++ src/main/java/net/theevilreaper/bounce/util/GameMessages.java
@@ -1,6 +1,7 @@
 package net.theevilreaper.bounce.util;
 
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.minimessage.tag.Tag;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.minestom.server.entity.Player;
@@ -11,12 +12,18 @@ import org.jetbrains.annotations.NotNull;
 
 public class GameMessages extends Messages {
 
-    private static final Component LEAVE_PART;
-    private static final Component JOIN_PART;
-
     public static final Component ALREADY_FORCE_STARTED;
     public static final Component PHASE_NOT_RUNNING;
     public static final Component PHASE_FORCE_STARTED;
+
+    public static final Component STATS_LINE;
+
+    private static final Component LEAVE_PART;
+    private static final Component JOIN_PART;
+
+    private static final Component PLUS_POINTS;
+    private static final Component MINUS_POINTS;
+    private static final Component POINT_PART;
 
     static {
         LEAVE_PART = withMini("<gray>left the game!");
@@ -27,6 +34,12 @@ public class GameMessages extends Messages {
         PHASE_NOT_RUNNING = withMiniPrefix("<red>The lobby countdown is not running!");
         PHASE_FORCE_STARTED = withMiniPrefix("<gray>The timer has been set to <color:#09ff00><seconds></color> seconds!",
                 TagResolver.builder().tag("seconds", (argumentQueue, context) -> Tag.preProcessParsed(String.valueOf(forceStartTime))).build());
+
+        PLUS_POINTS = Component.text("+", NamedTextColor.YELLOW).append(Component.space());
+        MINUS_POINTS = Component.text("-", NamedTextColor.RED).append(Component.space());
+        POINT_PART = Component.space().append(Component.text("Points", NamedTextColor.GRAY));
+
+        STATS_LINE = Component.text("━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\n", NamedTextColor.GRAY);
     }
 
     @Contract
@@ -49,6 +62,12 @@ public class GameMessages extends Messages {
     public static @NotNull Component getLeaveMessage(@NotNull Player player) {
         return withPrefix(withMini("<color:#249D9F>" + player.getUsername() + "</color>"))
                 .append(Component.space()).append(LEAVE_PART);
+    }
+
+    @Contract(value = "_, _ -> new", pure = true)
+    public static @NotNull Component getCoinComponent(int coins, boolean add) {
+        Component base = add ? PLUS_POINTS : MINUS_POINTS;
+        return base.append(Component.text(coins, NamedTextColor.GOLD)).append(POINT_PART);
     }
 
     private GameMessages() {
