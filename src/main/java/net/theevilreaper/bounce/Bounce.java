2bd637d2928478ce080356c392a7fa41724ab257
theEvilReaper <steffenwx@gmail.com>
2025-06-20 12:47:43 +0200

    Update method calls and more

--- src/main/java/net/theevilreaper/bounce/Bounce.java
+++ src/main/java/net/theevilreaper/bounce/Bounce.java
@@ -1,21 +1,32 @@
 package net.theevilreaper.bounce;
 
 import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.player.PlayerDisconnectEvent;
+import net.minestom.server.event.player.PlayerSpawnEvent;
+import net.theevilreaper.aves.map.MapProvider;
 import net.theevilreaper.bounce.commands.StartCommand;
 import net.theevilreaper.bounce.common.ListenerHandling;
 import net.theevilreaper.bounce.common.config.GameConfig;
 import net.theevilreaper.bounce.common.config.GameConfigReader;
-import net.theevilreaper.bounce.map.MapManager;
+import net.theevilreaper.bounce.event.BounceGameFinishEvent;
+import net.theevilreaper.bounce.listener.PlayerConfigurationListener;
+import net.theevilreaper.bounce.listener.PlayerJoinListener;
+import net.theevilreaper.bounce.listener.PlayerQuitListener;
+import net.theevilreaper.bounce.listener.game.GameFinishListener;
+import net.theevilreaper.bounce.map.BounceMapProvider;
 import net.theevilreaper.bounce.profile.BounceProfile;
+import net.theevilreaper.bounce.profile.ProfileService;
 import net.theevilreaper.bounce.timer.PlayingPhase;
 import net.theevilreaper.bounce.timer.LobbyPhase;
 import net.theevilreaper.bounce.timer.RestartPhase;
 import net.theevilreaper.bounce.timer.TeleportPhase;
-import net.theevilreaper.bounce.util.GameUtil;
 import net.theevilreaper.bounce.util.ItemUtil;
+import net.theevilreaper.bounce.util.PlayerUtil;
 import net.theevilreaper.bounce.util.ScoreboardUtil;
 import net.theevilreaper.xerus.api.phase.LinearPhaseSeries;
 import net.theevilreaper.xerus.api.phase.Phase;
@@ -26,20 +37,22 @@ import java.nio.file.Paths;
 
 public class Bounce implements ListenerHandling {
 
-    private MapManager mapManager;
-    private ProfileService<BounceProfile> profileService;
-    private GameConfig gameConfig;
+    private ProfileService profileService;
+    private final GameConfig gameConfig;
     private ScoreboardUtil scoreboardUtil;
-    private GameUtil gameUtil;
-    private ItemUtil itemUtil;
+    private final ItemUtil itemUtil;
+    private final MapProvider mapProvider;
     private final LinearPhaseSeries<Phase> phaseSeries;
+    private final PlayerUtil playerUtil;
 
     public Bounce() {
         Path path = Paths.get("");
         this.gameConfig = new GameConfigReader(path.resolve("config")).getConfig();
-
+        this.mapProvider = new BounceMapProvider(path);
+        this.itemUtil = new ItemUtil();
         this.phaseSeries = new LinearPhaseSeries<>();
-
+        this.profileService = new ProfileService();
+        this.playerUtil = new PlayerUtil(this.profileService);
 
         this.registerCommands();
         this.registerPhases();
@@ -48,72 +61,59 @@ public class Bounce implements ListenerHandling {
     public void load() {
         GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
         registerCancelListener(globalEventHandler);
-        setPrefix("§7[§3Bounce§7] §r");
-        WorldHelper.disablePlayerFileSave();
         scoreboardUtil = new ScoreboardUtil();
-        mapManager = new MapManager(this);
-        itemUtil = new ItemUtil();
-        gameUtil = new GameUtil(this);
-        profileService = new ProfileService<>();
         registerCommands();
-        registerListener();
-        lobbyPhase.start();
-        // Cloud.getInstance().setSpigotState(SpigotState.AVAILABLE);
+        registerListener(globalEventHandler);
+
+        this.phaseSeries.start();
     }
 
     public void unload() {
-        //Cloud.getInstance().setSpigotState(SpigotState.END);
         scoreboardUtil = null;
-        mapManager = null;
-        itemUtil = null;
-        gameUtil = null;
-        profileService.getProfiles().clear();
+        profileService.clear();
         profileService = null;
     }
 
     private void registerPhases() {
         this.phaseSeries.add(new LobbyPhase(this.gameConfig.minPlayers(), this.gameConfig.maxPlayers(), this.gameConfig.lobbyTime()));
-        this.phaseSeries.add(new TeleportPhase(this.itemUtil, player -> { }));
-        this.phaseSeries.add(new PlayingPhase(value -> {}));
+        this.phaseSeries.add(new TeleportPhase(this.itemUtil, ((BounceMapProvider) this.mapProvider)::teleportToGameSpawn));
+        this.phaseSeries.add(new PlayingPhase(value -> {
+        }));
         this.phaseSeries.add(new RestartPhase());
     }
 
     private void registerListener(@NotNull EventNode<Event> node) {
+        node.addListener(AsyncPlayerConfigurationEvent.class, new PlayerConfigurationListener(
+                this.phaseSeries::getCurrentPhase,
+                this.mapProvider.getActiveInstance(),
+                gameConfig.maxPlayers())
+        );
+        node.addListener(PlayerSpawnEvent.class, new PlayerJoinListener(this.phaseSeries::getCurrentPhase, player -> {
+            this.mapProvider.teleportToSpawn(player, true);
+        }));
+
+        node.addListener(PlayerDisconnectEvent.class, new PlayerQuitListener(
+                this.phaseSeries::getCurrentPhase, this::handleGameLeave
+        ));
+    }
+
+    private void registerGameListener(@NotNull EventNode<Event> node) {
+        node.addListener(BounceGameFinishEvent.class, new GameFinishListener(playerUtil));
     }
 
     private void registerCommands() {
         MinecraftServer.getCommandManager().register(new StartCommand(this.phaseSeries::getCurrentPhase));
     }
 
-    public MapManager getMapManager() {
-        return mapManager;
-    }
+    private void handleGameLeave(@NotNull Player player) {
+        BounceProfile profile = this.profileService.remove(player);
 
-    public GameUtil getGameUtil() {
-        return gameUtil;
-    }
+        if (profile == null) return;
 
-    public ItemUtil getItemUtil() {
-        return itemUtil;
+        profile.getJumpRunnable().cancel();
     }
 
     public ScoreboardUtil getScoreboardUtil() {
         return scoreboardUtil;
-    }
-
-    public ProfileService<BounceProfile> getProfileService() {
-        return profileService;
-    }
-
-    public LobbyPhase getLobbyTimer() {
-        return lobbyPhase;
-    }
-
-    public PlayingPhase getGameTimer() {
-        return gameTimer;
-    }
-
-    public RestartPhase getRestartTimer() {
-        return restartTimer;
     }
 }
