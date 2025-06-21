f8f055177a0ca8d599b112b23c9c417c1381c32a
theEvilReaper <steffenwx@gmail.com>
2025-06-21 23:07:54 +0200

    Add integration test for the ItemUtil

---
+++ src/test/java/net/theevilreaper/bounce/util/ItemUtiIntegrationTest.java
@@ -0,0 +1,61 @@
+package net.theevilreaper.bounce.util;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.item.ItemComponent;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
+import net.minestom.server.item.component.EnchantmentList;
+import net.minestom.server.item.enchant.Enchantment;
+import net.minestom.testing.Env;
+import net.minestom.testing.extension.MicrotusExtension;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MicrotusExtension.class)
+class ItemUtiIntegrationTest {
+
+    @Test
+    void testItemStackSet(@NotNull Env env) {
+        Instance instance = env.createFlatInstance();
+        Player player = env.createPlayer(instance);
+
+        assertEquals(ItemStack.AIR, player.getInventory().getItemStack(0));
+
+        ItemUtil itemUtil = new ItemUtil();
+
+        // Test setting an item stack
+        itemUtil.setItem(player);
+
+        ItemStack itemStack = player.getInventory().getItemStack(0);
+
+        // Verify that the item stack is not null and has the expected properties
+        assertNotEquals(ItemStack.AIR, itemStack);
+        assertEquals(Material.FEATHER, itemStack.material());
+
+        assertTrue(itemStack.has(ItemComponent.CUSTOM_NAME));
+
+        assertTrue(itemStack.has(ItemComponent.ENCHANTMENTS));
+
+        Component customName = itemStack.get(ItemComponent.CUSTOM_NAME);
+        assertNotNull(customName);
+        String displayName = PlainTextComponentSerializer.plainText().serialize(customName);
+
+        assertNotNull(displayName);
+        assertTrue(displayName.contains("Recoil-pushing recoil pusher"));
+
+        EnchantmentList enchantmentList = itemStack.get(ItemComponent.ENCHANTMENTS);
+
+        assertNotNull(enchantmentList);
+        assertEquals(1, enchantmentList.enchantments().size());
+        assertTrue(enchantmentList.has(Enchantment.KNOCKBACK));
+
+        env.destroyInstance(instance, true);
+    }
+
+}
