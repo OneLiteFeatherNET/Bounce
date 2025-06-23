414eefe4ee6b57f5145e03d81e05307e6f6c4ec3
Joltras <MinnyMouseXD@gmail.com>
2025-06-23 15:45:44 +0200

    More listener handling

--- setup/src/main/java/net/theevilreaper/bounce/setup/BounceSetup.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/BounceSetup.java
@@ -5,23 +5,35 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.event.Event;
 import net.minestom.server.event.EventNode;
 import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.instance.AddEntityToInstanceEvent;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
 import net.minestom.server.event.player.PlayerSpawnEvent;
 import net.minestom.server.event.player.PlayerUseItemEvent;
+import net.minestom.server.instance.Instance;
 import net.minestom.server.tag.Tag;
 import net.onelitefeather.guira.SetupDataService;
+import net.onelitefeather.guira.event.SetupFinishEvent;
+import net.theevilreaper.aves.file.FileHandler;
+import net.theevilreaper.aves.file.GsonFileHandler;
 import net.theevilreaper.aves.map.MapProvider;
+import net.theevilreaper.aves.util.functional.PlayerConsumer;
 import net.theevilreaper.bounce.common.ListenerHandling;
+import net.theevilreaper.bounce.common.util.GsonUtil;
 import net.theevilreaper.bounce.setup.data.BounceData;
+import net.theevilreaper.bounce.setup.event.MapSetupSelectEvent;
 import net.theevilreaper.bounce.setup.inventory.MapSetupInventory;
 import net.theevilreaper.bounce.setup.listener.PlayerConfigurationListener;
 import net.theevilreaper.bounce.setup.listener.PlayerItemListener;
 import net.theevilreaper.bounce.setup.listener.PlayerSpawnListener;
+import net.theevilreaper.bounce.setup.listener.entity.EntityAddToInstanceListener;
+import net.theevilreaper.bounce.setup.listener.map.MapSetupSelectListener;
+import net.theevilreaper.bounce.setup.listener.map.SetupFinishListener;
 import net.theevilreaper.bounce.setup.map.BounceSetupMapProvider;
 import net.theevilreaper.bounce.setup.util.SetupItems;
 import org.jetbrains.annotations.NotNull;
 
 import java.nio.file.Path;
+import java.util.function.Supplier;
 
 public final class BounceSetup implements ListenerHandling {
 
@@ -30,6 +42,7 @@ public final class BounceSetup implements ListenerHandling {
     private final SetupDataService<BounceData> setupDataService;
     private final MapSetupInventory mapSetupInventory;
     private final SetupItems setupItems;
+    private final FileHandler fileHandler;
 
     public BounceSetup() {
         Path path = Path.of("");
@@ -37,7 +50,7 @@ public final class BounceSetup implements ListenerHandling {
         this.setupDataService = SetupDataService.create();
         this.mapSetupInventory = new MapSetupInventory(this.mapProvider::getEntries);
         this.setupItems = new SetupItems();
-
+        this.fileHandler = new GsonFileHandler(GsonUtil.GSON);
         MinecraftServer.getSchedulerManager().buildShutdownTask(this::onShutdown);
     }
 
@@ -54,12 +67,28 @@ public final class BounceSetup implements ListenerHandling {
     }
 
     private void registerListener(@NotNull EventNode<Event> node) {
-        node.addListener(AsyncPlayerConfigurationEvent.class, new PlayerConfigurationListener(this.mapProvider.getActiveInstance()));
+        Supplier<Instance> instanceSupplier = this.mapProvider.getActiveInstance();
+        node.addListener(AsyncPlayerConfigurationEvent.class, new PlayerConfigurationListener(instanceSupplier));
         node.addListener(PlayerUseItemEvent.class, new PlayerItemListener(this::openMapSetupInventory, this.setupDataService::get));
         node.addListener(PlayerSpawnEvent.class, new PlayerSpawnListener(
                 this.setupItems::setOverViewItem,
                 player -> this.mapProvider.teleportToSpawn(player, false))
         );
+
+        node.addListener(MapSetupSelectEvent.class, new MapSetupSelectListener(this.fileHandler, this.setupDataService));
+
+        PlayerConsumer instanceSwitcher = player -> {
+            mapProvider.teleportToSpawn(player, true);
+            setupItems.setOverViewItem(player);
+        };
+
+        node.addListener(
+                (Class<SetupFinishEvent<BounceData>>) (Class<?>) SetupFinishEvent.class,
+                new SetupFinishListener(instanceSwitcher)
+        );
+
+        node.addListener(AddEntityToInstanceEvent.class, new EntityAddToInstanceListener(instanceSupplier, setupItems));
+
     }
 
     private void openMapSetupInventory(@NotNull Player player) {
