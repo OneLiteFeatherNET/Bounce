40a271ca40035c3eb61e9f52fdedea7c8d7a3a14
theEvilReaper <steffenwx@gmail.com>
2025-06-24 12:01:44 +0200

    Mark each variable as final and remove setters

--- common/src/main/java/net/theevilreaper/bounce/common/map/GameMap.java
+++ common/src/main/java/net/theevilreaper/bounce/common/map/GameMap.java
@@ -2,14 +2,15 @@ package net.theevilreaper.bounce.common.map;
 
 import net.minestom.server.coordinate.Pos;
 import net.theevilreaper.aves.map.BaseMap;
+import org.jetbrains.annotations.NotNull;
 
 public class GameMap extends BaseMap {
 
-    private Pos gameSpawn;
-    private double defaultPush;
-    private double emeraldPush;
-    private double ironPush;
-    private double goldPush;
+    private final Pos gameSpawn;
+    private final double defaultPush;
+    private final double emeraldPush;
+    private final double ironPush;
+    private final double goldPush;
 
     public GameMap(String name, Pos spawn, Pos gameSpawn, double defaultPush, double emeraldPush, double ironPush, double goldPush) {
         super(name, spawn, "Team");
@@ -18,26 +19,6 @@ public class GameMap extends BaseMap {
         this.emeraldPush = emeraldPush;
         this.ironPush = ironPush;
         this.goldPush = goldPush;
-    }
-
-    public void setDefaultPush(double defaultPush) {
-        this.defaultPush = defaultPush;
-    }
-
-    public void setEmeraldPush(double emeraldPush) {
-        this.emeraldPush = emeraldPush;
-    }
-
-    public void setGameSpawn(Pos gameSpawn) {
-        this.gameSpawn = gameSpawn;
-    }
-
-    public void setGoldPush(double goldPush) {
-        this.goldPush = goldPush;
-    }
-
-    public void setIronPush(double ironPush) {
-        this.ironPush = ironPush;
     }
 
     public double getDefaultPush() {
@@ -56,7 +37,7 @@ public class GameMap extends BaseMap {
         return ironPush;
     }
 
-    public Pos getGameSpawn() {
+    public @NotNull Pos getGameSpawn() {
         return gameSpawn;
     }
 }
