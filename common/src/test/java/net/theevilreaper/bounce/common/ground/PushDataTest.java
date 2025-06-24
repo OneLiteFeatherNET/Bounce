d29d1e45a3ceaa1c1be5afa2b88fb5db26643ffa
theEvilReaper <steffenwx@gmail.com>
2025-06-24 11:52:22 +0200

    Add new test case

--- common/src/test/java/net/theevilreaper/bounce/common/ground/PushDataTest.java
+++ common/src/test/java/net/theevilreaper/bounce/common/ground/PushDataTest.java
@@ -24,4 +24,28 @@ class PushDataTest {
         // Test for a block not in the map and expect 0.0
         assertEquals(0.0, pushData.getPush(Block.SAND));
     }
+
+    @Test
+    void testPushDataBuilder() {
+        PushData.Builder builder = PushData.builder();
+        builder.add(Block.STONE, 1.0)
+               .add(Block.DIRT, 0.5)
+               .add(Block.GRASS_BLOCK, 0.75);
+
+        PushData pushData = builder.build();
+
+        assertNotNull(pushData);
+        assertEquals(1.0, pushData.getPush(Block.STONE));
+        assertEquals(0.5, pushData.getPush(Block.DIRT));
+        assertEquals(0.75, pushData.getPush(Block.GRASS_BLOCK));
+        // Test for a block not in the map and expect 0.0
+        assertEquals(0.0, pushData.getPush(Block.SAND));
+
+        PushData.Builder anotherBuilder = PushData.builder(pushData);
+        anotherBuilder.remove(Block.STONE);
+        PushData anotherPushData = anotherBuilder.build();
+        assertNotNull(anotherPushData);
+        assertEquals(0.0, anotherPushData.getPush(Block.STONE)); // Stone should be removed
+        assertNotEquals(pushData, anotherPushData);
+    }
 }
