9765183a14e1909010329d1e79f886717d9c5f5a
theEvilReaper <steffenwx@gmail.com>
2025-06-24 16:27:43 +0200

    Add GameMapBuilder

---
+++ setup/src/main/java/net/theevilreaper/bounce/setup/builder/GameMapBuilder.java
@@ -0,0 +1,20 @@
+package net.theevilreaper.bounce.setup.builder;
+
+import net.minestom.server.coordinate.Pos;
+import net.theevilreaper.bounce.common.ground.PushData;
+import net.theevilreaper.bounce.common.map.GameMap;
+import org.jetbrains.annotations.NotNull;
+
+public class GameMapBuilder {
+
+    private String name;
+    private String[] authors;
+    private Pos spawn;
+    private Pos gameSpawn;
+    private PushData.Builder pushDataBuilder;
+
+
+    public @NotNull GameMap build() {
+
+    }
+}
