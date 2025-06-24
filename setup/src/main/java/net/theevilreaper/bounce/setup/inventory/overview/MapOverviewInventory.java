82c77ca157c674f8d6b4e879be8ba1417d3bdb57
Joltras <MinnyMouseXD@gmail.com>
2025-06-24 21:17:11 +0200

    Add slot usage to the inventory

--- setup/src/main/java/net/theevilreaper/bounce/setup/inventory/overview/MapOverviewInventory.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/inventory/overview/MapOverviewInventory.java
@@ -5,21 +5,51 @@ import net.minestom.server.entity.Player;
 import net.minestom.server.inventory.InventoryType;
 import net.theevilreaper.aves.inventory.InventoryLayout;
 import net.theevilreaper.aves.inventory.PersonalInventoryBuilder;
+import net.theevilreaper.aves.inventory.slot.ISlot;
 import net.theevilreaper.aves.inventory.util.LayoutCalculator;
-import net.theevilreaper.aves.util.functional.PlayerConsumer;
-import net.theevilreaper.bounce.setup.inventory.slot.BackSlot;
+import net.theevilreaper.bounce.setup.builder.GameMapBuilder;
+import net.theevilreaper.bounce.setup.inventory.slot.GroundSlot;
+import net.theevilreaper.bounce.setup.inventory.slot.MultiStringSlot;
+import net.theevilreaper.bounce.setup.inventory.slot.PositionSlot;
+import net.theevilreaper.bounce.setup.inventory.slot.StringSlot;
 import net.theevilreaper.bounce.setup.util.SetupItems;
 import org.jetbrains.annotations.NotNull;
 
 public final class MapOverviewInventory extends PersonalInventoryBuilder {
 
-    public MapOverviewInventory(@NotNull Player player, @NotNull PlayerConsumer backFunction) {
-        super(Component.text("Data view"), InventoryType.CHEST_3_ROW, player);
+    private static final int[] DATA_SLOT = LayoutCalculator.from(9, 11, 13, 15, 17);
 
+    private final GameMapBuilder builder;
+
+    public MapOverviewInventory(@NotNull Player player, @NotNull GameMapBuilder builder) {
+        super(Component.text("Data view"), InventoryType.CHEST_3_ROW, player);
+        this.builder = builder;
         InventoryLayout layout = InventoryLayout.fromType(getType());
 
-        layout.setItems(LayoutCalculator.quad(0, getType().getSize()), SetupItems.DECORATION);
-        layout.setItem(getType().getSize(), new BackSlot(), (player1, i, clickType, result) -> backFunction.accept(player));
+        layout.setItems(LayoutCalculator.quad(0, getType().getSize() - 1), SetupItems.DECORATION);
+
+        this.setDataLayoutFunction(dataLayoutFunction -> {
+            InventoryLayout dataLayout = dataLayoutFunction == null ? InventoryLayout.fromType(getType()) : dataLayoutFunction;
+            dataLayout.blank(DATA_SLOT);
+
+            OverviewType[] overviewTypes = OverviewType.getValues();
+
+            for (int i = 0; i < overviewTypes.length && i < DATA_SLOT.length; i++) {
+                OverviewType currentType = overviewTypes[i];
+                dataLayout.setItem(DATA_SLOT[i], getOverViewItem(currentType));
+            }
+            return dataLayout;
+        });
         this.setLayout(layout);
+    }
+
+    private @NotNull ISlot getOverViewItem(@NotNull OverviewType type) {
+        return switch (type) {
+            case SPAWN -> new PositionSlot(type, this.builder.getSpawn());
+            case GAME_SPAWN -> new PositionSlot(type, this.builder.getGameSpawn());
+            case GROUND -> new GroundSlot(type);
+            case NAME -> new StringSlot(type, builder.getName());
+            case BUILDER -> new MultiStringSlot(type, builder.getAuthors());
+        };
     }
 }
