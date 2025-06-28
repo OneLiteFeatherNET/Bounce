36fc0f18c98dfe4fc5d82da6ae2329c7f71151a2
theEvilReaper <steffenwx@gmail.com>
2025-06-28 10:34:09 +0200

    Add PushData usage to the GameMap

--- common/src/main/java/net/theevilreaper/bounce/common/map/GameMap.java
+++ common/src/main/java/net/theevilreaper/bounce/common/map/GameMap.java
@@ -2,41 +2,52 @@ package net.theevilreaper.bounce.common.map;
 
 import net.minestom.server.coordinate.Pos;
 import net.theevilreaper.aves.map.BaseMap;
+import net.theevilreaper.bounce.common.push.PushData;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
-public class GameMap extends BaseMap {
+/**
+ * The {@link GameMap} contains all relevant information about a map which is used in the context of the game.
+ * It holds data about the used positions and other things.
+ *
+ * @author theEvilReaper
+ * @version 1.1.0
+ * @since 0.1.0
+ */
+@ApiStatus.NonExtendable
+public final class GameMap extends BaseMap {
 
     private final Pos gameSpawn;
-    private final double defaultPush;
-    private final double emeraldPush;
-    private final double ironPush;
-    private final double goldPush;
+    private final PushData pushData;
 
-    public GameMap(String name, Pos spawn, Pos gameSpawn, double defaultPush, double emeraldPush, double ironPush, double goldPush) {
+    /**
+     * Creates a new reference from the map class.
+     *
+     * @param name      the name of the map
+     * @param spawn     the spawn position
+     * @param gameSpawn the spawn position during the game
+     * @param pushData  the {@link PushData} which includes information about push values
+     */
+    public GameMap(@NotNull String name, @NotNull Pos spawn, @NotNull Pos gameSpawn, @NotNull PushData pushData) {
         super(name, spawn, "Team");
         this.gameSpawn = gameSpawn;
-        this.defaultPush = defaultPush;
-        this.emeraldPush = emeraldPush;
-        this.ironPush = ironPush;
-        this.goldPush = goldPush;
+        this.pushData = pushData;
     }
 
-    public double getDefaultPush() {
-        return defaultPush;
+    /**
+     * Returns the given {@link PushData} reference.
+     *
+     * @return the reference
+     */
+    public @NotNull PushData getPushData() {
+        return this.pushData;
     }
 
-    public double getEmeraldPush() {
-        return emeraldPush;
-    }
-
-    public double getGoldPush() {
-        return goldPush;
-    }
-
-    public double getIronPush() {
-        return ironPush;
-    }
-
+    /**
+     * Returns the position which is used during the game.
+     *
+     * @return the game spawn position
+     */
     public @NotNull Pos getGameSpawn() {
         return gameSpawn;
     }
