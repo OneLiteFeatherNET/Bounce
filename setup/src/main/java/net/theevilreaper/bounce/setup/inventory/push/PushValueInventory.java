475bb4c3cdb4ef08acef4a1b9875fc06ba81d6ae
Joltras <MinnyMouseXD@gmail.com>
2025-07-01 22:49:42 +0200

    Add inventory classes related to push entries

---
+++ setup/src/main/java/net/theevilreaper/bounce/setup/inventory/push/PushValueInventory.java
@@ -0,0 +1,111 @@
+package net.theevilreaper.bounce.setup.inventory.push;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.EventDispatcher;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.inventory.InventoryType;
+import net.minestom.server.inventory.click.ClickType;
+import net.minestom.server.inventory.condition.InventoryConditionResult;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
+import net.theevilreaper.aves.inventory.InventoryLayout;
+import net.theevilreaper.aves.inventory.PersonalInventoryBuilder;
+import net.theevilreaper.aves.inventory.util.LayoutCalculator;
+import net.theevilreaper.bounce.setup.builder.GameMapBuilder;
+import net.theevilreaper.bounce.setup.event.SetupInventorySwitchEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
+import static net.theevilreaper.bounce.setup.util.SetupItems.DECORATION;
+
+public final class PushValueInventory extends PersonalInventoryBuilder {
+
+    private static final Component TITLE = Component.text("Push Value");
+    private static final Component DISPLAY_NAME = Component.text("Boost Value", NamedTextColor.GREEN);
+    private static final Component CURRENT_VALUE = Component.text("Current:", NamedTextColor.GRAY).append(Component.space());
+    private static final Component LEFT_CLICK = miniMessage().deserialize("<yellow>Left-click</yellow><gray>: </gray><green>+1 value</green>");
+    private static final Component RIGHT_CLICK = miniMessage().deserialize("<yellow>Right-click</yellow><gray>: </gray><red>-1 value</red>");
+
+    private static final int BLOCK_SLOT = 11;
+    private static final int VALUE_SLOT = 15;
+
+    private Material material;
+    private int value = 0;
+
+    public PushValueInventory(@NotNull Player player, @NotNull GameMapBuilder gameMapBuilder, @NotNull Block block, int value) {
+        super(TITLE, InventoryType.CHEST_3_ROW, player);
+        this.material = block.registry().material();
+        this.value = value;
+
+        InventoryLayout layout = InventoryLayout.fromType(getType());
+        layout.setItems(LayoutCalculator.quad(0, getType().getSize() - 1), DECORATION);
+
+        this.setLayout(layout);
+
+        this.setDataLayoutFunction(dataLayoutFunction -> {
+            InventoryLayout dataLayout = dataLayoutFunction == null ? InventoryLayout.fromType(getType()) : dataLayoutFunction;
+
+            dataLayout.blank(LayoutCalculator.from(BLOCK_SLOT, VALUE_SLOT));
+
+            dataLayout.setItem(BLOCK_SLOT, ItemStack.builder(material).build(), this::handleBlockClick);
+            dataLayout.setItem(VALUE_SLOT, getPushValue(), this::handlePushButtonClick);
+
+            return dataLayout;
+        });
+
+        this.setCloseFunction(closeFunction -> {
+            if (dataLayoutValid) return;
+            this.handleClose(gameMapBuilder);
+        });
+        this.register();
+    }
+
+    private void handleClose(@NotNull GameMapBuilder gameMapBuilder) {
+        gameMapBuilder.getPushDataBuilder().add(this.material.block(), this.value);
+        this.invalidateDataLayout();
+    }
+
+    private void handleBlockClick(@NotNull Player player, int slot, @NotNull ClickType type, @NotNull InventoryConditionResult result) {
+        result.setCancel(true);
+        player.closeInventory();
+        EventDispatcher.call(new SetupInventorySwitchEvent(player, SetupInventorySwitchEvent.SwitchTarget.PUSH_LAYER));
+    }
+
+    private void handlePushButtonClick(@NotNull Player player, int slot, @NotNull ClickType type, @NotNull InventoryConditionResult result) {
+        result.setCancel(true);
+
+        if (type != ClickType.LEFT_CLICK && type != ClickType.RIGHT_CLICK) {
+            return;
+        }
+
+        int oldValue = value;
+        if (type == ClickType.LEFT_CLICK) {
+            value++;
+        } else if (value > 0) {
+            value--;
+        }
+
+        if (value != oldValue) {
+            this.invalidateDataLayout();
+        }
+    }
+
+    private @NotNull ItemStack getPushValue() {
+        List<Component> lore = new ArrayList<>();
+        lore.add(Component.empty());
+        lore.add(CURRENT_VALUE.append(Component.text(value, NamedTextColor.YELLOW)));
+        lore.add(Component.empty());
+        lore.add(LEFT_CLICK);
+        lore.add(RIGHT_CLICK);
+        lore.add(Component.empty());
+        return ItemStack.builder(Material.FEATHER)
+                .customName(DISPLAY_NAME)
+                .lore(lore)
+                .build();
+    }
+}
