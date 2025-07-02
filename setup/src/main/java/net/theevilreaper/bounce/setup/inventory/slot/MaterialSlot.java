866084d1427b618c4b4f750e00546933db27902c
Joltras <MinnyMouseXD@gmail.com>
2025-07-02 17:24:45 +0200

    Add additional constructor

--- setup/src/main/java/net/theevilreaper/bounce/setup/inventory/slot/MaterialSlot.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/inventory/slot/MaterialSlot.java
@@ -12,10 +12,24 @@ public class MaterialSlot extends Slot {
 
     private final ItemStack stack;
 
+    /**
+     * Creates a new {@link MaterialSlot} with the specified {@link Material}.
+     *
+     * @param material the {@link Material} to be used for this slot, typically representing a block or item.
+     */
     public MaterialSlot(@NotNull Material material) {
         this.stack = ItemStack.builder(material)
                 .lore(Component.empty(), SetupMessages.NO_SPACE_SEPARATOR.append(Component.space()).append(Component.text("Ground block", NamedTextColor.GRAY)))
                 .build();
+    }
+
+    /**
+     * Creates a new {@link MaterialSlot} with the specified {@link ItemStack}.
+     *
+     * @param stack the {@link ItemStack} to be used for this slot, typically representing a material.
+     */
+    public MaterialSlot(@NotNull ItemStack stack) {
+        this.stack = stack;
     }
 
     @Override
