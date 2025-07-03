89b056611382b40d868cd98e72ed17905188cfca
Joltras <MinnyMouseXD@gmail.com>
2025-07-03 13:20:13 +0200

    Update context usage of the listener

--- setup/src/main/java/net/theevilreaper/bounce/setup/listener/ground/PlayerBlockSelectListener.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/listener/ground/PlayerBlockSelectListener.java
@@ -3,9 +3,10 @@ package net.theevilreaper.bounce.setup.listener.ground;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.item.Material;
+import net.theevilreaper.bounce.common.push.PushData;
 import net.theevilreaper.bounce.setup.builder.GameMapBuilder;
 import net.theevilreaper.bounce.setup.data.BounceData;
-import net.theevilreaper.bounce.setup.event.PlayerBlockSelectEvent;
+import net.theevilreaper.bounce.setup.event.ground.PlayerGroundBlockSelectEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
@@ -13,7 +14,7 @@ import java.util.UUID;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
-public class PlayerBlockSelectListener implements Consumer<PlayerBlockSelectEvent> {
+public class PlayerBlockSelectListener implements Consumer<PlayerGroundBlockSelectEvent> {
 
     private final Function<UUID, Optional<BounceData>> bounceDataSupplier;
 
@@ -22,7 +23,7 @@ public class PlayerBlockSelectListener implements Consumer<PlayerBlockSelectEven
     }
 
     @Override
-    public void accept(@NotNull PlayerBlockSelectEvent event) {
+    public void accept(@NotNull PlayerGroundBlockSelectEvent event) {
         Optional<BounceData> data = bounceDataSupplier.apply(event.getPlayer().getUuid());
 
         if (data.isEmpty()) return;
@@ -31,12 +32,7 @@ public class PlayerBlockSelectListener implements Consumer<PlayerBlockSelectEven
         Material material = event.getMaterial();
         System.out.println("Player " + event.getPlayer().getUsername() + " selected block: " + material.name());
         Block block = material.block();
-
-        if (event.getPart() == PlayerBlockSelectEvent.GroundPart.BLOCK) {
-            this.handleGroundBlockChange(bounceData, block);
-            return;
-        }
-        this.handlePushBlockChange(bounceData, block);
+        this.handleGroundBlockChange(bounceData, block);
     }
 
     private void handleGroundBlockChange(@NotNull BounceData bounceData, @NotNull Block block) {
@@ -49,8 +45,17 @@ public class PlayerBlockSelectListener implements Consumer<PlayerBlockSelectEven
 
     private void handlePushBlockChange(@NotNull BounceData bounceData, @NotNull Block block) {
         GameMapBuilder mapBuilder = bounceData.getMapBuilder();
+        if (hasBlock(mapBuilder.getPushDataBuilder(), block)) {
+            System.out.println("Block " + block.name() + " already exists in push data.");
+            return;
+        }
         mapBuilder.getPushDataBuilder().add(block, 0);
         bounceData.triggerPushViewUpdate();
         MinecraftServer.getSchedulerManager().scheduleNextTick(() -> bounceData.backToGroundView(true));
+    }
+
+    private boolean hasBlock(@NotNull PushData.Builder pushDataBuilder, @NotNull Block block) {
+        return pushDataBuilder.getPushValues().stream()
+                .anyMatch(pushBlock -> pushBlock.getBlock().equals(block));
     }
 }
