9033eb22689fdd88cbc2dd2a15a8891f13c8ec4b
theEvilReaper <steffenwx@gmail.com>
2025-07-09 21:40:17 +0200

    Cleanup code base

--- setup/src/main/java/net/theevilreaper/bounce/setup/inventory/ground/GroundValueInventory.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/inventory/ground/GroundValueInventory.java
@@ -1,7 +1,6 @@
 package net.theevilreaper.bounce.setup.inventory.ground;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventDispatcher;
 import net.minestom.server.inventory.InventoryType;
@@ -17,21 +16,14 @@ import net.theevilreaper.bounce.setup.builder.GameMapBuilder;
 import net.theevilreaper.bounce.setup.event.SetupInventorySwitchEvent;
 import net.theevilreaper.bounce.setup.event.SetupInventorySwitchEvent.SwitchTarget;
 import net.theevilreaper.bounce.setup.inventory.slot.SwitchTargetSlot;
+import net.theevilreaper.bounce.setup.util.LoreHelper;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static net.kyori.adventure.text.minimessage.MiniMessage.miniMessage;
 import static net.theevilreaper.bounce.setup.util.SetupItems.DECORATION;
 
 public class GroundValueInventory extends PersonalInventoryBuilder {
 
     private static final Component TITLE = Component.text("Ground block");
-    private static final Component DISPLAY_NAME = Component.text("Boost Value", NamedTextColor.GREEN);
-    private static final Component CURRENT_VALUE = Component.text("Current:", NamedTextColor.GRAY).append(Component.space());
-    private static final Component LEFT_CLICK = miniMessage().deserialize("<yellow>Left-click</yellow><gray>: </gray><green>+1 value</green>");
-    private static final Component RIGHT_CLICK = miniMessage().deserialize("<yellow>Right-click</yellow><gray>: </gray><red>-1 value</red>");
 
     private static final int BLOCK_SLOT = 11;
     private static final int VALUE_SLOT = 15;
@@ -47,16 +39,13 @@ public class GroundValueInventory extends PersonalInventoryBuilder {
 
         this.setLayout(layout);
         this.pushEntry = gameMapBuilder.getGroundBlockEntry();
+
         this.setDataLayoutFunction(dataLayoutFunction -> {
             InventoryLayout dataLayout = dataLayoutFunction == null ? InventoryLayout.fromType(getType()) : dataLayoutFunction;
-
-
             dataLayout.blank(LayoutCalculator.from(BLOCK_SLOT, VALUE_SLOT));
             Material material = pushEntry.getBlock().registry().material();
-
             dataLayout.setItem(BLOCK_SLOT, ItemStack.builder(material).build(), this::handleBlockClick);
-            dataLayout.setItem(VALUE_SLOT, getPushValue(), this::handlePushButtonClick);
-
+            dataLayout.setItem(VALUE_SLOT, LoreHelper.getPushValue(pushEntry), this::handlePushButtonClick);
             return dataLayout;
         });
     }
@@ -64,7 +53,7 @@ public class GroundValueInventory extends PersonalInventoryBuilder {
     private void handleBlockClick(@NotNull Player player, int i, @NotNull ClickType clickType, @NotNull InventoryConditionResult result) {
         result.setCancel(true);
         player.closeInventory();
-        EventDispatcher.call(new SetupInventorySwitchEvent(player, SwitchTarget.GROUND_LAYER));
+        EventDispatcher.call(new SetupInventorySwitchEvent(player, SwitchTarget.GROUND_BLOCK_LAYER));
     }
 
     private void handlePushButtonClick(@NotNull Player player, int slot, @NotNull ClickType type, @NotNull InventoryConditionResult result) {
@@ -84,19 +73,5 @@ public class GroundValueInventory extends PersonalInventoryBuilder {
         if (this.pushEntry.getValue() != oldValue) {
             this.invalidateDataLayout();
         }
-    }
-
-    private @NotNull ItemStack getPushValue() {
-        List<Component> lore = new ArrayList<>();
-        lore.add(Component.empty());
-        lore.add(CURRENT_VALUE.append(Component.text(this.pushEntry.getValue(), NamedTextColor.YELLOW)));
-        lore.add(Component.empty());
-        lore.add(LEFT_CLICK);
-        lore.add(RIGHT_CLICK);
-        lore.add(Component.empty());
-        return ItemStack.builder(Material.FEATHER)
-                .customName(DISPLAY_NAME)
-                .lore(lore)
-                .build();
     }
 }
