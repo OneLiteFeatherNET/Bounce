ae86c2c2b4917c07c260e877e13211031a6eebb6
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-09 22:56:37 +0200

    Add documentation

--- setup/src/main/java/net/theevilreaper/bounce/setup/inventory/overview/MapOverviewInventory.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/inventory/overview/MapOverviewInventory.java
@@ -16,6 +16,14 @@ import net.theevilreaper.bounce.setup.inventory.slot.StringSlot;
 import net.theevilreaper.bounce.setup.util.SetupItems;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * The {@link MapOverviewInventory} is a custom inventory implementation of the {@link PersonalInventoryBuilder} class.
+ * It provides an overview of the information of a {@link GameMapBuilder} instance.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public final class MapOverviewInventory extends PersonalInventoryBuilder {
 
     private static final int[] DATA_SLOT = LayoutCalculator.from(9, 11, 13, 15, 17);
@@ -23,6 +31,13 @@ public final class MapOverviewInventory extends PersonalInventoryBuilder {
     private final GameMapBuilder builder;
     private final VoidConsumer viewSwitcher;
 
+    /**
+     * Creates a new MapOverviewInventory for the given player.
+     *
+     * @param player       the {@link Player} who is involved
+     * @param builder      the {@link GameMapBuilder} which contains the map data
+     * @param viewSwitcher a {@link VoidConsumer} to switch the view when the ground slot is clicked
+     */
     public MapOverviewInventory(@NotNull Player player, @NotNull GameMapBuilder builder, @NotNull VoidConsumer viewSwitcher) {
         super(Component.text("Data view"), InventoryType.CHEST_3_ROW, player);
         this.builder = builder;
@@ -46,6 +61,12 @@ public final class MapOverviewInventory extends PersonalInventoryBuilder {
         this.setLayout(layout);
     }
 
+    /**
+     * Maps a {@link OverviewType} to a specific {@link ISlot}.
+     *
+     * @param type the {@link OverviewType} to map
+     * @return the corresponding {@link ISlot} for the given type
+     */
     private @NotNull ISlot getOverViewItem(@NotNull OverviewType type) {
         return switch (type) {
             case SPAWN -> new PositionSlot(type, this.builder.getSpawn());
