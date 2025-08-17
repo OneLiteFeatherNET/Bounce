8bb2effbb6f1abe870ac565f75cde812f45ce744
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-17 11:41:58 +0200

    Remove NonExtendable annotation usage

--- setup/src/main/java/net/theevilreaper/bounce/setup/util/SetupTags.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/util/SetupTags.java
@@ -3,7 +3,6 @@ package net.theevilreaper.bounce.setup.util;
 import net.minestom.server.tag.Tag;
 import org.jetbrains.annotations.ApiStatus;
 
-@ApiStatus.NonExtendable
 @ApiStatus.Internal
 public final class SetupTags {
 
