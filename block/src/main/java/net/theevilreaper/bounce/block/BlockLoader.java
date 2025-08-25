d87219c6afbb6bdf364c3d8c365d5046cdb8b24e
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-25 12:07:51 +0200

    Work on the loader part

--- block/src/main/java/net/theevilreaper/bounce/block/BlockLoader.java
+++ block/src/main/java/net/theevilreaper/bounce/block/BlockLoader.java
@@ -1,4 +1,28 @@
 package net.theevilreaper.bounce.block;
 
-public class BlockLoader {
+import net.minestom.server.instance.block.BlockManager;
+import org.jetbrains.annotations.Contract;
+
+import net.theevilreaper.bounce.block.type.lantern.LanternBlockFactory.Type;
+
+public sealed interface BlockLoader permits BlockLoaderBuilder {
+
+    /**
+     * Creates a new {@link BlockLoaderBuilder} for the specified block manager.
+     *
+     * @param blockManager the block manager to use
+     * @return a new {@link BlockLoaderBuilder} instance
+     */
+    @Contract(value = "_ -> new", pure = true)
+    static BlockLoader builder(BlockManager blockManager) {
+        return new BlockLoaderBuilder(blockManager);
+    }
+
+    /**
+     * Registers a new torch variant.
+     *
+     * @param type the type of the torch variant
+     * @return this builder instance for chaining
+     */
+    BlockLoader lanternVariant(Type type);
 }
