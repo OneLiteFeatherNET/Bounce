976f14eb3330295744bf7b1f1ca3ef2a53865acd
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-10-03 22:13:02 +0200

    Add CloudNet integration

--- src/main/java/net/theevilreaper/bounce/BounceServer.java
+++ src/main/java/net/theevilreaper/bounce/BounceServer.java
@@ -1,5 +1,8 @@
 package net.theevilreaper.bounce;
 
+import dev.derklaro.aerogel.Injector;
+import eu.cloudnetservice.driver.inject.InjectionLayer;
+import eu.cloudnetservice.modules.bridge.impl.platform.minestom.MinestomBridgeExtension;
 import io.github.togar2.pvp.MinestomPvP;
 import net.minestom.server.MinecraftServer;
 
@@ -10,6 +13,9 @@ public class BounceServer {
         MinestomPvP.init();
         Bounce bounce = new Bounce();
         bounce.load();
+        try (InjectionLayer<Injector> layer = InjectionLayer.ext()) {
+            layer.instance(MinestomBridgeExtension.class).onLoad();
+        }
         minecraftServer.start("localhost", 25565);
     }
 }
