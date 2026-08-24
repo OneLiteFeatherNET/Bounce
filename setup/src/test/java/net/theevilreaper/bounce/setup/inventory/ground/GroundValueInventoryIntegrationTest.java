5a8c20ee012cfbb10dc94a8a49aa62aa2d918b96
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-08-24 23:24:44 +0200

    test(value): add new weight test

---
+++ setup/src/test/java/net/theevilreaper/bounce/setup/inventory/ground/GroundValueInventoryIntegrationTest.java
@@ -0,0 +1,42 @@
+package net.theevilreaper.bounce.setup.inventory.ground;
+
+import net.minestom.server.entity.Player;
+import net.minestom.server.instance.Instance;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
+import net.minestom.testing.Env;
+import net.minestom.testing.extension.MicrotusExtension;
+import net.theevilreaper.aves.inventory.layout.InventoryLayout;
+import net.theevilreaper.aves.inventory.slot.ISlot;
+import net.theevilreaper.bounce.setup.builder.GameMapBuilder;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MicrotusExtension.class)
+class GroundValueInventoryIntegrationTest {
+
+    private static final int WEIGHT_SLOT = 13;
+
+    @Test
+    void testWeightSlotShowsCurrentWeight(@NotNull Env env) {
+        Instance instance = env.createFlatInstance();
+        Player player = env.createPlayer(instance);
+        GameMapBuilder gameMapBuilder = new GameMapBuilder();
+
+        GroundValueInventory inventory = new GroundValueInventory(player, gameMapBuilder);
+        inventory.open();
+        env.tick();
+
+        InventoryLayout dataLayout = inventory.getDataLayout();
+        ISlot slot = dataLayout.getSlot(WEIGHT_SLOT);
+        assertNotNull(slot);
+        ItemStack item = slot.getItem();
+        assertNotNull(item);
+        assertEquals(Material.NETHER_STAR, item.material());
+
+        env.destroyInstance(instance, true);
+    }
+}
