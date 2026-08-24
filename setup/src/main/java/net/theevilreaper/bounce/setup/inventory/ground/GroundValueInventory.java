68adac24c0431f4f460dcceb057e43cd7cdb278a
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-08-24 23:19:45 +0200

    chore(setup): add weight option

--- setup/src/main/java/net/theevilreaper/bounce/setup/inventory/ground/GroundValueInventory.java
+++ setup/src/main/java/net/theevilreaper/bounce/setup/inventory/ground/GroundValueInventory.java
@@ -13,11 +13,11 @@ import net.theevilreaper.aves.inventory.click.ClickHolder;
 import net.theevilreaper.aves.inventory.util.LayoutCalculator;
 import net.theevilreaper.bounce.common.push.PushEntry;
 import net.theevilreaper.bounce.setup.builder.GameMapBuilder;
+import net.theevilreaper.bounce.setup.dialog.event.PlayerDialogRequestEvent;
 import net.theevilreaper.bounce.setup.event.SetupInventorySwitchEvent;
 import net.theevilreaper.bounce.setup.event.SetupInventorySwitchEvent.SwitchTarget;
 import net.theevilreaper.bounce.setup.inventory.slot.SwitchTargetSlot;
 import net.theevilreaper.bounce.setup.util.LoreHelper;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Consumer;
 
@@ -28,11 +28,12 @@ public class GroundValueInventory extends PersonalInventoryBuilder {
     private static final Component TITLE = Component.text("Ground block");
 
     private static final int BLOCK_SLOT = 11;
+    private static final int WEIGHT_SLOT = 13;
     private static final int VALUE_SLOT = 15;
 
     private final PushEntry pushEntry;
 
-    public GroundValueInventory(@NotNull Player player, @NotNull GameMapBuilder gameMapBuilder) {
+    public GroundValueInventory(Player player, GameMapBuilder gameMapBuilder) {
         super(TITLE, InventoryType.CHEST_3_ROW, player);
         InventoryLayout layout = InventoryLayout.fromType(getType());
         layout.setItems(LayoutCalculator.quad(0, getType().getSize() - 1), DECORATION);
@@ -47,18 +48,19 @@ public class GroundValueInventory extends PersonalInventoryBuilder {
             dataLayout.blank(LayoutCalculator.from(BLOCK_SLOT, VALUE_SLOT));
             Material material = pushEntry.getBlock().material();
             dataLayout.setItem(BLOCK_SLOT, ItemStack.builder(material).build(), this::handleBlockClick);
+            dataLayout.setItem(WEIGHT_SLOT, LoreHelper.getWeight(pushEntry), this::handleWeightButtonClick);
             dataLayout.setItem(VALUE_SLOT, LoreHelper.getPushValue(pushEntry), this::handlePushButtonClick);
             return dataLayout;
         });
     }
 
-    private void handleBlockClick(@NotNull Player player, int slot, @NotNull Click clickType, @NotNull ItemStack stack, @NotNull Consumer<ClickHolder> result) {
+    private void handleBlockClick(Player player, int slot, Click clickType, ItemStack stack, Consumer<ClickHolder> result) {
         result.accept(ClickHolder.cancelClick());
         player.closeInventory();
         EventDispatcher.call(new SetupInventorySwitchEvent(player, SwitchTarget.GROUND_BLOCKS_OVERVIEW));
     }
 
-    private void handlePushButtonClick(@NotNull Player player, int slot, @NotNull Click click, @NotNull ItemStack stack, @NotNull Consumer<ClickHolder> result) {
+    private void handlePushButtonClick(Player player, int slot, Click click, ItemStack stack, Consumer<ClickHolder> result) {
         result.accept(ClickHolder.cancelClick());
         if ((!(click instanceof Click.Left || click instanceof Click.Right))) return;
 
@@ -72,5 +74,13 @@ public class GroundValueInventory extends PersonalInventoryBuilder {
         if (this.pushEntry.getValue() != oldValue) {
             this.invalidateDataLayout();
         }
+    }
+
+    private void handleWeightButtonClick(Player player, int slot, Click click, ItemStack stack, Consumer<ClickHolder> result) {
+        result.accept(ClickHolder.cancelClick());
+        if ((!(click instanceof Click.Left || click instanceof Click.Right))) return;
+
+        player.setTag(net.theevilreaper.bounce.setup.util.SetupTags.PUSH_SLOT_INDEX, 0);
+        EventDispatcher.call(new PlayerDialogRequestEvent(player, PlayerDialogRequestEvent.Target.SETUP_BLOCK_WEIGHT));
     }
 }
