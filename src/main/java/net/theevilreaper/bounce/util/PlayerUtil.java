ec5946a17dddf246da3ad39239959b9300cca9af
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-09-07 09:44:17 +0200

    chore(util): remove constructor usage

--- src/main/java/net/theevilreaper/bounce/util/PlayerUtil.java
+++ src/main/java/net/theevilreaper/bounce/util/PlayerUtil.java
@@ -2,25 +2,16 @@ package net.theevilreaper.bounce.util;
 
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.entity.Player;
-import net.theevilreaper.bounce.common.push.PushData;
-import net.theevilreaper.bounce.profile.BounceProfile;
-import net.theevilreaper.bounce.profile.ProfileService;
+import net.theevilreaper.bounce.player.BouncePlayer;
 
 public final class PlayerUtil {
-
-    private final ProfileService profileService;
-    private final PushData pushData;
-
-    public PlayerUtil(ProfileService profileService, PushData pushData) {
-        this.profileService = profileService;
-        this.pushData = pushData;
-    }
 
     public void preparePlayers() {
         for (Player onlinePlayer : MinecraftServer.getConnectionManager().getOnlinePlayers()) {
             onlinePlayer.setLevel(0);
-            BounceProfile profile = this.profileService.add(onlinePlayer);
-            profile.registerJumpRunnable(pushData);
+            if (onlinePlayer instanceof BouncePlayer bouncePlayer) {
+                bouncePlayer.startRound();
+            }
         }
     }
 }
