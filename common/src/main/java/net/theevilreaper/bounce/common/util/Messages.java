3dc2cae2596b302d9073f8d9dd09264eecc8c14e
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-09-17 12:58:08 +0200

    chore(message): remove unused method and add documentation

--- common/src/main/java/net/theevilreaper/bounce/common/util/Messages.java
+++ common/src/main/java/net/theevilreaper/bounce/common/util/Messages.java
@@ -6,44 +6,65 @@ import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import org.jetbrains.annotations.Contract;
 
+/**
+ * Utility class for creating messages with the Bounce prefix.
+ * <p>
+ * Messages can either be created from an existing {@link Component} or from
+ * MiniMessage formatted text.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 0.1.0
+ */
 public abstract class Messages {
 
     public static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
-    private static final Component PREFIX;
+
     public static final Component SEPARATOR;
 
+    private static final Component PREFIX;
+
     static {
-        SEPARATOR = Component.space().append(Component.text("»", NamedTextColor.GRAY)).append(Component.space());
-        PREFIX = Component.text("Bounce", NamedTextColor.DARK_AQUA).append(SEPARATOR);
+        SEPARATOR = Component.space()
+                .append(Component.text("»", NamedTextColor.GRAY))
+                .append(Component.space());
+        PREFIX = Component.text("Bounce", NamedTextColor.DARK_AQUA)
+                .append(SEPARATOR);
     }
 
-    protected Messages() { }
-
-    @Contract(value = "_ -> new", pure = true)
-    public static Component withPrefix(String component) {
-        return PREFIX.append(MINI_MESSAGE.deserialize(component));
+    protected Messages() {
     }
 
+    /**
+     * Adds the Bounce prefix to the given component.
+     *
+     * @param component to append
+     * @return a new component containing the prefix and the given component
+     */
     @Contract(value = "_ -> new", pure = true)
     public static Component withPrefix(Component component) {
         return PREFIX.append(component);
     }
 
-    @Contract(value = "_ -> new", pure = true)
-    public static Component withMini(String text) {
-        return MINI_MESSAGE.deserialize(text);
-    }
-
-    @Contract(value = "_, _ -> new", pure = true)
-    public static Component withMini(String text, TagResolver... resolvers) {
-        return MINI_MESSAGE.deserialize(text, resolvers);
-    }
-
+    /**
+     * Deserializes the given MiniMessage text and adds the Bounce prefix.
+     *
+     * @param text to append
+     * @return a new component containing the prefix and the deserialized text
+     */
     @Contract(value = "_ -> new", pure = true)
     public static Component withMiniPrefix(String text) {
         return PREFIX.append(MINI_MESSAGE.deserialize(text));
     }
 
+    /**
+     * Deserializes the given MiniMessage text with the provided tag resolvers
+     * and adds the Bounce prefix.
+     *
+     * @param text      to append
+     * @param resolvers the tag resolvers used during deserialization
+     * @return a new component containing the prefix and the deserialized text
+     */
     @Contract(value = "_, _ -> new", pure = true)
     public static Component withMiniPrefix(String text, TagResolver... resolvers) {
         return PREFIX.append(MINI_MESSAGE.deserialize(text, resolvers));
